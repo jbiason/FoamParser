2df1a25ed002ebec27bd0704039dad327beb65a1
Julio Biason <j.biason@engys.com>
2023-02-22 16:45:37 -0300

    Let's do this recursively

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,6 @@
 //! Parse a Foam file into a major structure.
 
-use std::collections::HashMap;
-
+use pest::iterators::Pair;
 use pest::Parser;
 use pest_derive::Parser;
 
@@ -11,20 +10,21 @@ struct FoamParser;
 
 #[derive(Debug, PartialEq)]
 pub enum FoamElement {
-    Values(Vec<String>),
+    Element(String),
+    Attribution(String, Vec<FoamElement>),
     List(Vec<String>),
 }
 
 /// The main Foam parser.
 #[derive(Debug, PartialEq)]
 pub struct Foam {
-    root: HashMap<String, FoamElement>,
+    root: Vec<FoamElement>,
 }
 // Note: I could done this inside the enum, but I don't want to expose the
 //       HashMap to the user.
 
-impl AsRef<HashMap<String, FoamElement>> for Foam {
-    fn as_ref(&self) -> &HashMap<String, FoamElement> {
+impl AsRef<Vec<FoamElement>> for Foam {
+    fn as_ref(&self) -> &Vec<FoamElement> {
         &self.root
     }
 }
@@ -48,37 +48,42 @@ impl TryFrom<&str> for Foam {
         let file = FoamParser::parse(Rule::file, value)?
             .next()
             .ok_or(FoamError::EmptyFile)?;
+        println!("Root: {:?}", file);
 
-        /* ?
-        .next()?; */
-        let mut root = HashMap::new();
+        let elements = file
+            .into_inner()
+            .map(|pair| Foam::parse_value(pair))
+            .collect::<Vec<FoamElement>>();
+        Ok(Foam { root: elements })
+    }
+}
 
-        for line in file.into_inner() {
-            match line.as_rule() {
-                Rule::attribution => {
-                    let mut inner_rules = line.into_inner();
-                    let name: &str = inner_rules.next().unwrap().as_str();
-                    let values = inner_rules
-                        .into_iter()
-                        .map(|x| x.as_str().to_string())
-                        .collect::<Vec<String>>();
-                    root.insert(name.into(), FoamElement::Values(values));
-                }
-                Rule::EOI => (),
-                Rule::list => {
-                    let mut inner_rules = line.into_inner();
-                    let name: &str = inner_rules.next().unwrap().as_str();
-                    println!("{:?}", inner_rules);
-                    let values = inner_rules
-                        .into_iter()
-                        .map(|x| x.as_str().to_string())
-                        .collect::<Vec<String>>();
-                    root.insert(name.into(), FoamElement::List(values));
-                }
-                r => panic!("I don't know what {r:?} is!"),
+impl Foam {
+    fn parse_value(pair: Pair<Rule>) -> FoamElement {
+        println!("{:?}", pair);
+        match pair.as_rule() {
+            Rule::attribution => {
+                println!("Attribution: {:?}", pair);
+                let mut inner_rules = pair.into_inner();
+                let definition =
+                    inner_rules.next().unwrap().as_str().to_string();
+                println!("Defition: {}", definition);
+                let elements = inner_rules
+                    .map(|pair| {
+                        println!("Att inner: {:?}", pair);
+                        Foam::parse_value(pair)
+                    })
+                    .collect::<Vec<FoamElement>>();
+                FoamElement::Attribution(definition, elements)
             }
+            Rule::value => {
+                println!("Value: {:?}", pair);
+                FoamElement::Element(pair.as_str().to_string())
+            }
+            Rule::list => todo!(),
+            Rule::element => todo!(),
+            r => panic!("Can't handle {r:?}"),
         }
-        Ok(Foam { root })
     }
 }
 
@@ -87,251 +92,24 @@ mod parser {
     use super::*;
 
     #[test]
-    fn multi_comment() {
-        let parse =
-            FoamParser::parse(Rule::multi_comment, "/* this is comment */");
-        assert!(parse.is_ok());
-    }
-
-    #[test]
-    fn definition() {
-        let parse = FoamParser::parse(Rule::definition, "foamFile");
-        assert!(parse.is_ok(), "{:#?}", parse);
-        let parse = FoamParser::parse(Rule::definition, "div(phi,U)");
-        assert!(parse.is_ok(), "{:#?}", parse);
-        let parse = FoamParser::parse(
-            Rule::definition,
-            "div((nuEff*dev2(T(grad(U)))))",
-        );
-        assert!(parse.is_ok(), "{:#?}", parse);
-        let parse = FoamParser::parse(Rule::definition, "FoamFile");
-        assert!(parse.is_ok(), "{:#?}", parse);
-        let parse = FoamParser::parse(Rule::definition, "foam_file");
-        assert!(parse.is_ok());
-    }
-
-    #[test]
     fn attribution() {
-        let parse = FoamParser::parse(Rule::attribution, "version 2.0;");
-        assert!(parse.is_ok(), "{:#?}", parse);
-        let parse = FoamParser::parse(
-            Rule::attribution,
-            "div(U)          Gauss linear;",
-        );
-        assert!(parse.is_ok(), "{:#?}", parse);
+        let result = FoamParser::parse(Rule::attribution, "a_var value;");
+        assert!(result.is_ok())
     }
-
-    #[test]
-    fn broken_multi() {
-        let parse = FoamParser::parse(Rule::multi_comment, "/* bad comment");
-        assert!(parse.is_err());
-    }
-
-    #[test]
-    fn single_comment() {
-        let parse =
-            FoamParser::parse(Rule::single_comment, "// this is comment");
-        assert!(parse.is_ok());
-    }
-
-    #[test]
-    fn broken_value() {
-        let parse = FoamParser::parse(Rule::value, "123");
-        assert!(parse.is_ok(), "{:?}", parse);
-
-        let parse = FoamParser::parse(Rule::value, "asd ");
-        assert!(parse.is_ok(), "{:?}", parse);
-    }
-
-    #[test]
-    fn list() {
-        let text = "list_name ( 1 2 3 );";
-        let parse = FoamParser::parse(Rule::list, text);
-        assert!(parse.is_ok(), "{:#?}", parse);
-    }
-
-    #[test]
-    fn short_list() {
-        let text = "a_list (1 2 3);";
-        let parse = FoamParser::parse(Rule::list, text);
-        assert!(parse.is_ok(), "{:#?}", parse);
-    }
-
-    #[test]
-    fn list_in_list() {
-        let text = "list_name ( (1 2 3) (4 5 6) );";
-        let parse = FoamParser::parse(Rule::list, text);
-        assert!(parse.is_ok(), "{:#?}", parse);
-    }
-
-    // #[test]
-    // fn dictionary() {
-    //     let text = "FoamFile\n{\nversion 2.0;\nformat ascii;\nclass dictionary;\nlocation system;\nobject caseSetupDict;\n}";
-    //     let parse = FoamParser::parse(Rule::dictionary, text);
-    //     assert!(parse.is_ok(), "{:#?}", parse);
-    // }
-
-    // #[test]
-    // fn dict_in_dict() {
-    //     let text = "dict1 { dict2 { class bad; } }";
-    //     let parse = FoamParser::parse(Rule::dictionary, text);
-    //     assert!(parse.is_ok(), "{:#?}", parse);
-    // }
-
-    // #[test]
-    // fn sized_list() {
-    //     let text = "list_name 3 ( 1 2 3 );";
-    //     let parse = FoamParser::parse(Rule::list, text);
-    //     assert!(parse.is_ok(), "{:#?}", parse);
-    // }
 }
 
-// #[cfg(test)]
-// mod file {
-// use super::*;
-// use pest::Parser;
-
-// #[test]
-// fn chained_comments() {
-//     let text = "/* this is one comment */\n// And this is another";
-//     let parse = FoamParser::parse(Rule::file, text);
-//     assert!(parse.is_ok(), "{:?}", parse);
-// }
-
-// #[test]
-// fn attribution_with_comment() {
-//     let text = "version 2.0; // this is good";
-//     let parse = FoamParser::parse(Rule::file, text);
-//     assert!(parse.is_ok(), "{:?}", parse);
-// }
-
-// #[test]
-// fn mid_comment() {
-//     let text = "version /* this is comment */ 2.0;";
-//     let parse = FoamParser::parse(Rule::file, text);
-//     assert!(parse.is_ok(), "{:?}", parse);
-// }
-
-// #[test]
-// fn complex() {
-//     let text = "/* this is file */\nList ( 1\n2 // this is ok\n 3);";
-//     let parse = FoamParser::parse(Rule::file, text);
-//     assert!(parse.is_ok(), "{:?}", parse);
-// }
-
-// // Note: the following files were retrieved from OpenFoam examples:
-// // https://develop.openfoam.com/Development/openfoam/-/tree/develop/tutorials/incompressible/pimpleFoam/RAS/rotatingFanInRoom/system
-// #[test]
-// fn control_dict() {
-//     let text = include_bytes!("../resources/controlDict");
-//     let parse = FoamParser::parse(Rule::file, &std::str::from_utf8(text).unwrap());
-//     assert!(parse.is_ok(), "{:?}", parse);
-// }
-
-// #[test]
-// fn block_mesh_dict() {
-//     let text = include_bytes!("../resources/blockMeshDict");
-//     let parse = FoamParser::parse(Rule::file, &std::str::from_utf8(text).unwrap());
-//     assert!(parse.is_ok(), "{:?}", parse);
-// }
-
-// // #[test]
-// // fn create_patch_dict() {
-// //     let text = include_bytes!("../resources/createPatchDict");
-// //     let parse = FoamParser::parse(Rule::file, &std::str::from_utf8(text).unwrap());
-// //     assert!(parse.is_ok(), "{:?}", parse);
-// // }
-
-// #[test]
-// fn fv_scheme() {
-//     let text = include_bytes!("../resources/fvSchemes");
-//     let parse = FoamParser::parse(Rule::file, &std::str::from_utf8(text).unwrap());
-//     assert!(parse.is_ok(), "{:?}", parse);
-// }
-// }
-
 #[cfg(test)]
-mod as_struct {
+mod foam_struct {
     use super::*;
 
     #[test]
-    fn just_attribs() {
-        let source = "version 2.0 1.0 0.0;";
-        let result = Foam::try_from(source).unwrap();
-        let expected = HashMap::from([(
-            String::from("version"),
-            FoamElement::Values(vec![
-                String::from("2.0"),
-                String::from("1.0"),
-                String::from("0.0"),
-            ]),
-        )]);
-        assert_eq!(result.as_ref(), &expected);
-    }
-
-    #[test]
-    fn two_attribs() {
-        let source = "version 2.0;format foam;";
-        let result = Foam::try_from(source).unwrap();
-        let expected = HashMap::from([
-            (
-                String::from("version"),
-                FoamElement::Values(vec![String::from("2.0")]),
-            ),
-            (
-                String::from("format"),
-                FoamElement::Values(vec![String::from("foam")]),
-            ),
-        ]);
-        assert_eq!(result.as_ref(), &expected);
-    }
-
-    #[test]
-    fn attribs_with_comments() {
-        let source = "version 2.0; // this is good;\nformat foam; /* but this is more gooder */";
-        let result = Foam::try_from(source).unwrap();
-        let expected = HashMap::from([
-            (
-                String::from("version"),
-                FoamElement::Values(vec![String::from("2.0")]),
-            ),
-            (
-                String::from("format"),
-                FoamElement::Values(vec![String::from("foam")]),
-            ),
-        ]);
-        assert_eq!(result.as_ref(), &expected);
-    }
-
-    #[test]
-    fn attribs_with_special_chars() {
-        let source = "fun_code 1;\np(U) 2;";
-        let result = Foam::try_from(source).unwrap();
-        let expected = HashMap::from([
-            (
-                String::from("fun_code"),
-                FoamElement::Values(vec![String::from("1")]),
-            ),
-            (
-                String::from("p(U)"),
-                FoamElement::Values(vec![String::from("2")]),
-            ),
-        ]);
-        assert_eq!(result.as_ref(), &expected);
-    }
-
-    #[test]
-    fn list() {
-        let source = "a_list (1 2 3);";
-        let result = Foam::try_from(source).unwrap();
-        let expected = HashMap::from([(
-            String::from("a_list"),
-            FoamElement::List(vec![
-                String::from("1"),
-                String::from("2"),
-                String::from("3"),
-            ]),
-        )]);
+    fn attribution() {
+        let text = "a_var value;";
+        let result = Foam::try_from(text).unwrap();
+        let expected = vec![FoamElement::Attribution(
+            String::from("a_var"),
+            vec![FoamElement::Element(String::from("value"))],
+        )];
         assert_eq!(result.as_ref(), &expected);
     }
 }
