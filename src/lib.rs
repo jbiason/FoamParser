c1ade663eba7b7fea3a8e819f796dfb7b7ec93e0
Julio Biason <j.biason@engys.com>
2023-02-23 11:51:28 -0300

    lists

--- src/lib.rs
+++ src/lib.rs
@@ -83,7 +83,7 @@ impl Foam {
                     .collect::<Vec<FoamElement>>();
                 FoamElement::List(values)
             }
-            r => panic!("Can't handle {r:?}"),
+            r => panic!("Can't handle \"{r:?}\""),
         }
     }
 }
@@ -220,6 +220,26 @@ mod foam_struct {
             vec![FoamElement::List(vec![
                 FoamElement::Element(String::from("1")),
                 FoamElement::Element(String::from("2")),
+            ])],
+        )];
+        assert_eq!(result.as_ref(), &expected);
+    }
+
+    #[test]
+    fn list_of_lists() {
+        let text = "var ((1 2) (4 5));";
+        let result = Foam::try_from(text).unwrap();
+        let expected = vec![FoamElement::Attribution(
+            String::from("var"),
+            vec![FoamElement::List(vec![
+                FoamElement::List(vec![
+                    FoamElement::Element(String::from("1")),
+                    FoamElement::Element(String::from("2")),
+                ]),
+                FoamElement::List(vec![
+                    FoamElement::Element(String::from("4")),
+                    FoamElement::Element(String::from("5")),
+                ]),
             ])],
         )];
         assert_eq!(result.as_ref(), &expected);
