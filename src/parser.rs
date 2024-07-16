d81f458342760e3580b48db8e52f37c974d16563
Julio Biason <j.biason@engys.com>
2024-07-16 11:56:01 -0300

    refactor: Removed a lot of code since get_dict is doing everything we need

--- src/parser.rs
+++ src/parser.rs
@@ -10,91 +10,7 @@ use crate::FoamError;
 
 pub fn parse<'a>(content: &'a str) -> Result<Foam<'a>, FoamError<'a>> {
     let mut lexer = Token::lexer(content);
-    let mut root = HashMap::new();
-
-    loop {
-        if let Some(key) = get_keyword(&mut lexer)? {
-            let value = get_value(&mut lexer, "root")?;
-            root.insert(key, value);
-        } else {
-            break;
-        }
-    }
-
-    Ok(Foam::Dictionary(root))
-}
-
-fn get_keyword<'a>(
-    lexer: &mut logos::Lexer<'a, Token<'a>>,
-) -> std::result::Result<Option<&'a str>, FoamError<'a>> {
-    loop {
-        match lexer.next() {
-            None => return Ok(None),
-            Some(Err(_)) => return Err(FoamError::EndOfContent),
-            Some(Ok(Token::Keyword(token))) => return Ok(Some(token)),
-            Some(Ok(Token::MultilineComment(_))) => continue,
-            Some(Ok(Token::Comment(_))) => continue,
-            Some(Ok(_)) => {
-                let token = lexer.slice();
-                let span = lexer.span();
-                return Err(FoamError::MissingKeyword {
-                    token,
-                    start: span.start,
-                    end: span.end,
-                });
-            }
-        }
-    }
-}
-
-fn get_value<'a>(
-    lexer: &mut logos::Lexer<'a, Token<'a>>,
-    structure: &'a str,
-) -> std::result::Result<Vec<Foam<'a>>, FoamError<'a>> {
-    let mut result = Vec::new();
-    loop {
-        match lexer.next() {
-            // Input ended before we got all values
-            None => {
-                // There is a compromise here: `dict { dict value; }` would make the first dict
-                // being recognized and added to the list, but the end token (;) isn't used at the
-                // end of dictionaries, so although we process everything, the parser will bail out
-                // with an error.
-                //
-                // On the other hand, `var value` is an error ('cause attributions do require the
-                // end token) but if we end here there will be no harm done, anyway.
-                break;
-            }
-            Some(Err(_)) => return Err(FoamError::EndOfContent),
-
-            // Tokens that are closing some structure that we are not processing anyway.
-            Some(Ok(Token::ListEnd)) => {
-                return Err(FoamError::UnexpectedToken {
-                    token: ")",
-                    structure,
-                })
-            }
-            Some(Ok(Token::DictEnd)) => {
-                return Err(FoamError::UnexpectedToken {
-                    token: "}",
-                    structure,
-                })
-            }
-
-            // Tokens we are ignoring 'cause we don't keep them in our structures.
-            Some(Ok(Token::MultilineComment(_))) => continue,
-            Some(Ok(Token::Comment(_))) => continue,
-
-            // Acceptable tokens
-            Some(Ok(Token::Keyword(token))) => result.push(Foam::Value(token)),
-            Some(Ok(Token::DictStart)) => result.push(get_dict(lexer)?),
-            Some(Ok(Token::ListStart)) => result.push(get_list(lexer)?),
-
-            // Proper ending
-            Some(Ok(Token::End)) => break,
-        }
-    }
-    Ok(result)
+    Ok(get_dict(&mut lexer)?)
 }
 
 fn get_dict<'a>(
@@ -105,7 +21,7 @@ fn get_dict<'a>(
     let mut key_values = Vec::new();
     loop {
         match lexer.next() {
-            None => return Err(FoamError::EndOfContent),
+            None => break,
             Some(Err(_)) => return Err(FoamError::EndOfContent),
 
             Some(Ok(Token::ListEnd)) => {
@@ -129,7 +45,7 @@ fn get_dict<'a>(
                     structure: "dictionary",
                 })
             }
-            // `;` is acceptable only if we are alredy processing a list of values, like 
+            // `;` is acceptable only if we are alredy processing a list of values, like
             // `dict { list 1 2 3; }`. If we see the `;` and we are not processing a list, then
             // something is wrong.
             Some(Ok(Token::End)) if key.is_none() => {
@@ -152,7 +68,6 @@ fn get_dict<'a>(
 
             Some(Ok(Token::Keyword(token))) if key.is_none() => {
                 key = Some(token);
-                key_values.clear();
             }
             Some(Ok(Token::Keyword(token))) => {
                 key_values.push(Foam::Value(token))
@@ -161,13 +76,14 @@ fn get_dict<'a>(
             Some(Ok(Token::DictStart)) => key_values.push(get_dict(lexer)?),
 
             Some(Ok(Token::DictEnd)) => {
-                if let Some(key) = key {
-                    result.insert(key, key_values);
-                }
                 break;
             }
         }
     }
+    if let Some(key) = key {
+        result.insert(key, key_values);
+    }
+
     Ok(Foam::Dictionary(result))
 }
 
@@ -203,84 +119,12 @@ fn get_list<'a>(
             Some(Ok(Token::ListEnd)) => break,
         }
     }
-
-    // Just one small note: Lists are followed by ';', which we need to consume to not confused the
-    // parent caller. Also note that ';' is the only acceptable follower of the end of a list.
-    match lexer.next() {
-        Some(Ok(Token::End)) => (), // this is ok
-        None => return Err(FoamError::EndOfContent),
-        Some(Err(_)) => return Err(FoamError::EndOfContent),
-        Some(Ok(_token)) => {
-            return Err(FoamError::UnexpectedToken {
-                token: "?",
-                structure: "list",
-            })
-        }
-    }
-
     Ok(Foam::List(result))
 }
 
 #[cfg(test)]
 mod test {
     use super::*;
-
-    #[test]
-    fn keyword_direct() {
-        let mut lexer = Token::lexer("token");
-        let result = get_keyword(&mut lexer).unwrap();
-        assert_eq!(result, Some("token"));
-    }
-
-    #[test]
-    fn keyword_with_tabs() {
-        let mut lexer = Token::lexer("\t\t\ttoken");
-        let result = get_keyword(&mut lexer).unwrap();
-        assert_eq!(result, Some("token"));
-    }
-
-    #[test]
-    fn keyword_with_a_comment() {
-        let mut lexer = Token::lexer("// this is a comment\ntoken");
-        let result = get_keyword(&mut lexer).unwrap();
-        assert_eq!(result, Some("token"));
-    }
-
-    #[test]
-    fn keyword_with_multiline_comment() {
-        let mut lexer =
-            Token::lexer("/* this is comment\nmultiline, actually*/\ntoken");
-        let result = get_keyword(&mut lexer).unwrap();
-        assert_eq!(result, Some("token"));
-    }
-
-    #[test]
-    fn keyword_quoted() {
-        let mut lexer = Token::lexer("\"this is token\"");
-        let result = get_keyword(&mut lexer).unwrap();
-        assert_eq!(result, Some("this is token"));
-    }
-
-    #[test]
-    fn keyword_empty_input() {
-        let mut lexer = Token::lexer("");
-        let result = get_keyword(&mut lexer);
-        assert_eq!(result, Ok(None));
-    }
-
-    #[test]
-    fn keyword_wrong_token() {
-        let mut lexer = Token::lexer("{ inDict value; }");
-        let result = get_keyword(&mut lexer);
-        assert_eq!(
-            result,
-            Err(FoamError::MissingKeyword {
-                token: "{",
-                start: 0,
-                end: 1
-            })
-        )
-    }
 
     #[test]
     fn single_attribution() {
@@ -371,5 +215,22 @@ mod test {
         )]);
         let outer = HashMap::from([("outer", vec![Foam::Dictionary(inner)])]);
         assert_eq!(result, Ok(Foam::Dictionary(outer)));
+    }
+
+    #[test]
+    fn all_types() {
+        let result = parse("attribution 1;list (1 2);dict {inner 1;}");
+        let attribution = vec![Foam::Value("1")];
+        let list = Foam::List(vec![Foam::Value("1"), Foam::Value("2")]);
+        let dict = Foam::Dictionary(HashMap::from([(
+            "inner",
+            vec![Foam::Value("1")],
+        )]));
+        let main = Foam::Dictionary(HashMap::from([
+            ("attribution", attribution),
+            ("list", vec![list]),
+            ("dict", vec![dict]),
+        ]));
+        assert_eq!(result, Ok(main));
     }
 }
